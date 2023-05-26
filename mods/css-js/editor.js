d6b2455ecfea99b5163a978db4e3a8347d088392
PoggyCraft <134660557+PoggyCraft@users.noreply.github.com>
2023-05-25 22:24:55 -0300

    Add files via upload

---
+++ mods/css-js/editor.js
@@ -0,0 +1,228 @@
+﻿//Infos
+let docu = [];
+let indc;
+let numb;
+
+//Snapshot
+const loadMods = bankOf.collection("Modpack Atual");
+const coleccao = bankOf.collection('Modpack Atual');
+
+loadMods.orderBy("indice", "asc").onSnapshot((snapThis)=>{
+  docu = [];
+  indc = snapThis.size;
+  if(indc == 0){
+    numb = 0;
+  }else{
+    numb = snapThis.docs[snapThis.size - 1].data().indice;
+  }
+  snapThis.forEach((snap, index)=>{
+    const box = snap.data();
+    docu.push(box);
+    playArray();
+    if(snap === "added"){
+      loadInfos(box, index);
+      playArray();
+    }
+    if(snap === "modified"){
+      loadInfos(box, index);
+      playArray();
+    }
+    if(snap === "removed"){
+      playArray();
+    }
+  })
+});
+
+function playArray(){
+  Laucher.innerHTML = '';
+  Mods.innerHTML    = '';
+  Misc.innerHTML    = '';
+  let docuReverso = docu.slice();
+  docu.forEach((snapo, index)=>{
+    let card     = document.createElement('DIV');
+    let title    = document.createElement('input');
+    let desc     = document.createElement('textarea');
+    let obs      = document.createElement('input');
+    let time     = document.createElement('input');
+    let download = document.createElement('input');
+    let iconURL  = document.createElement('input');
+    let icone    = document.createElement('img');
+    let remove   = document.createElement('button');
+    let save     = document.createElement('button');
+    let removed  = document.createElement('button');
+    let local    = document.createElement('select');
+    let locales  = ["Mods", "Laucher", "Tributo", "Extra"];
+    
+    title.value      = snapo.nome;
+    desc.value       = snapo.descricao;
+    obs.value        = snapo.observacao;
+    icone.src        = snapo.icon;
+    removed.value    = snapo.removido;
+    time.value       = snapo.tempo;
+	
+	title.placeholder= 'Titulo';
+	desc.placeholder = 'Descrição';
+	obs.placeholder	 = 'Observação';
+    
+    //Array Inside
+    for(var x = 0; x < locales.length; x++){
+      let options = document.createElement('option');
+      options.innerHTML = locales[x];
+      local.append(options);
+    }
+    
+    local.value      = snapo.area;
+    
+    //Condições
+    iconURL.placeholder = 'Icone';
+    download.placeholder= 'Link De Download Do Mod';
+    time.placeholder    = 'Mod Preview';
+    
+    if(icone.src.search(/.jpg|.jpeg|.png|.gif|.com/) !== -1){
+      iconURL.value = snapo.icon;
+      icone.classList.remove('noImg');
+    }else{
+      iconURL.value = '';
+      icone.src = '';
+      icone.classList.add('noImg');
+    }
+    if(snapo.download.search(/.com|.org|.net|.br|.io/) !== -1){
+      download.value = snapo.download;
+    }else{
+      download.value = '';
+    }
+    card.append(icone, title, desc, obs, time, download, iconURL, removed, save, remove, local);
+    
+    //Place
+    
+    if(snapo.area === 'Mods'){
+      Mods.append(card);
+    }
+    else if(snapo.area === 'Laucher'){
+      Laucher.append(card);
+    }else{
+      Misc.append(card);
+    }
+    
+    //Variante
+    function remButton(){
+      if(removed.value == 0){
+        removed.classList.remove('rem');
+        card.classList.add('noMod');
+      }else{
+        removed.classList.add('rem');
+        card.classList.remove('noMod');
+      }
+    }
+    remButton();
+    
+    //Funções
+    function updateAll(){
+      loadMods.doc(snapo.doc).update({
+        nome:       title.value,
+        descricao:  desc.value,
+        download:   download.value,
+        observacao: obs.value,
+        area:       local.value,
+        icon:       iconURL.value,
+        removido:   removed.value,
+        tempo:      time.value,
+      });
+    }
+    
+    remove.addEventListener('click', ()=>{
+      loadMods.doc(snapo.doc).delete().then(()=>{});
+    });
+    
+    save.addEventListener('click', ()=>{
+      updateAll();
+    });
+    
+    removed.addEventListener('click', ()=>{
+      if(removed.value == 0){
+        removed.value = 1;
+      }else{
+        removed.value = 0;
+      }
+      updateAll();
+      remButton();
+    })
+    
+    iconURL.addEventListener('input', ()=>{
+      icone.src = iconURL.value;
+    });
+    local.addEventListener('change', ()=>{
+      // loadMods.doc(snapo.doc).update({
+      //   area: local.value
+      // })
+      updateAll();
+    });
+    
+    icone.addEventListener('click', ()=>{
+      iconURL.focus();
+      iconURL.select();
+    });
+    time.addEventListener('input', ()=>{
+      time.value = time.value.replace('https://youtu.be/', 'https://www.youtube.com/embed/');
+      time.value = time.value.replace('?t=', '?start=');
+    });
+  });
+}
+
+//ADD BOX
+addMod.addEventListener('click', ()=>{
+  console.log(parseInt(indc) + 1);
+  const novoDoc = `Mod ${parseInt(numb) + 1}`;
+  loadMods.doc(novoDoc).set({
+    nome:       'Titulo',
+    descricao:  '',
+    doc:        novoDoc,
+    download:   '',
+    observacao: '',
+    area:       'Mods',
+    icon:       '',
+    removido:   1,
+    indice:     parseInt(numb) + 1,
+    tempo:      '',
+  });
+  setTimeout(()=>{
+    let xDD = document.querySelector('#Mods div:last-of-type input:first-of-type');
+    xDD.focus();
+  }, 50);
+});
+
+
+//Update Page
+
+const dataUpdate = bankOf.collection('update');
+const updateName = document.querySelector('#updateVersion input');
+const updateDate = document.querySelector('#updateVersion p');
+
+updateName.addEventListener('input', ()=>{
+  updateName.value = updateName.value.toUpperCase();
+});
+
+dataUpdate.doc('update').onSnapshot((slap)=>{
+  let updateInfos = slap.data();
+  if(localStorage.getItem('updateIs') == undefined ||localStorage.getItem('updateIs') == null){
+	  localStorage.setItem('updateIs', 'none');
+  }
+  localStorage.setItem('updateIs', updateInfos.code);
+  updateName.value     = updateInfos.code;
+  updateDate.innerHTML = updateInfos.date;
+  
+  updateName.addEventListener('change', ()=>{
+    let data = new Date();
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth()).padStart(2, '0');
+    let ano = data.getFullYear();
+    let hora= String(data.getHours()).padStart(2, '0');
+    let min = String(data.getMinutes()).padStart(2, '0');
+    let dateFormat = `${dia}/${mes}/${ano} as ${hora}:${min}`;
+    console.log(dia);
+    dataUpdate.doc('update').update({
+      code: updateName.value,
+      date: dateFormat,
+    });
+  });
+});
---
+++ mods/css-js/page.js
@@ -0,0 +1,192 @@
+﻿//Update Local
+const dataUpdate = bankOf.collection('update');
+const updateName = document.querySelector('#updateVersion input');
+const updateDate = document.querySelector('#updateVersion p');
+const updateZone = document.querySelector('#updateVersion');
+const updateDesc = document.querySelector('#UpdateInformation p');
+
+let updateIs   = localStorage.getItem('updateIs');
+let lastUpdate = localStorage.getItem('lastUpdate');
+
+updateName.addEventListener('input', ()=>{
+  updateName.value = updateName.value.toUpperCase();
+});
+
+dataUpdate.doc('update').onSnapshot((slap)=>{
+  let updateInfos = slap.data();
+  updateName.value     = updateInfos.code;
+  updateDate.innerHTML = updateInfos.date;
+  localStorage.setItem('updateIs', updateInfos.code);
+  
+  updateZone.addEventListener('click', ()=>{
+    let updateIs   = localStorage.getItem('updateIs');
+    let lastUpdate = localStorage.getItem('lastUpdate');
+    console.log(updateIs, lastUpdate);
+    updateDesc.innerHTML = `Atualização Atual: ${updateIs} <br/>Sua Atualização: ${lastUpdate}`;
+    UpdateInformation.style.top = '0vw';
+  });
+  if(updateIs == null || lastUpdate == null){
+    localStorage.setItem('atual', updateInfos.code);
+    localStorage.setItem('lastUpdate', '0');
+  }
+});
+
+UpdateInformation.addEventListener('click', ()=>{
+  UpdateInformation.style.top = '';
+});
+
+//Real Script
+
+let datos = [];
+
+let indice;
+let lastest;
+let howMany;
+
+let downTown = bankOf.collection("Modpack Atual");
+
+storageBase();
+
+
+function storageBase(){
+  let updateIs   = localStorage.getItem('updateIs');
+  let lastUpdate = localStorage.getItem('lastUpdate');
+  
+  if(updateIs == lastUpdate){
+    datos = JSON.parse(localStorage.getItem('ListMod'));
+    if(datos == null){
+      setTimeout(()=>{
+        storageBase();
+      },500);
+    }else{
+      arrayThis();
+    }
+    console.log('Não Teve que baixar Dados');
+  }else{
+    getInformations();
+  }
+}
+
+
+function getInformations(){
+    downTown.orderBy("indice", "asc").onSnapshot((snop)=>{
+      datos = [];
+      howMany = snop.size;
+      
+      snop.forEach((snup, index)=>{
+        let panel = snup.data();
+        datos.push(panel);
+        localStorage.setItem("ListMod", JSON.stringify(datos));
+        arrayThis();
+        console.log('Teve que baixar Dados');
+        if(snup === 'added'){
+          arrayThis();
+        }
+        if(snup === 'modified'){
+          arrayThis();
+        }
+        localStorage.setItem('lastUpdate', updateIs);
+        setTimeout(()=>{
+          localStorage.setItem('lastUpdate', localStorage.getItem('updateIs'));
+        }, 500);
+      });
+    });
+}
+
+console.log(datos);
+if(datos == ''){
+  console.log(lastUpdate);
+  getInformations();
+}
+
+//   ARRAY
+
+function arrayThis(){
+  Content.innerHTML ='';
+  Titles.innerHTML = '';
+  RemovedMods.innerHTML = '';
+  FooterContent.innerHTML = '';
+  datos.forEach((panel)=>{
+    let box        = document.createElement('DIV');
+    let icon       = document.createElement('IMG');
+    let title      = document.createElement('H1');
+    let descricao  = document.createElement('H2');
+    let observacao = document.createElement('H3');
+    let download   = document.createElement('BUTTON');
+    
+    //content
+    title.innerHTML     = panel.nome;
+    descricao.innerHTML = panel.descricao;
+    observacao.innerHTML= panel.observacao;
+    icon.src            = panel.icon;
+    icon.setAttribute('draggable', 'false');
+    
+    //Conversores
+    // title.title = `${Math.floor(panel.tempo / 60)}:${(panel.tempo % 60 < 10 ? "0": "") + panel.tempo % 60}`;
+    
+    //Retornos
+    if(panel.icon.search(/.jpg|.jpeg|.png|.gif|.com/) !== -1){
+      box.append(icon);
+    }
+    box.append(title, descricao, observacao);
+    if(panel.download.search(/.com|.org|.br|.net|.io|.int/) == -1){
+    }else{
+      box.append(download);
+    }
+    
+    //Categorias
+    if(panel.area === 'Laucher'){
+      Titles.append(box);
+      if(panel.removido == '0'){
+        box.classList.add('removedPanel');
+      }else{
+        box.classList.remove('removedPanel');
+      }
+    }
+    if(panel.area === 'Mods' && panel.removido === '1'){
+	  if(panel.tempo !== ''){
+	  	title.title = `Preview do ${panel.nome}`;
+	  }
+      title.addEventListener('click', ()=>{
+        const ytVid = document.querySelector('#videoYoutube iframe');
+        const testo = document.getElementById('xDDD');
+		if(panel.tempo !== ''){
+			if(panel.tempo.includes('?t=') || panel.tempo.includes('?start=')){
+				testo.src = `${panel.tempo}&autoplay=1`;
+			}else{
+				testo.src = `${panel.tempo}?autoplay=1`;
+			}
+		}
+      });
+      Content.append(box);
+    }
+    if(panel.area === 'Mods' && panel.removido === '0'){
+	  if(panel.tempo !== ''){
+	  	title.title = `Preview do ${panel.nome}`;
+	  }
+      title.addEventListener('click', ()=>{
+        const ytVid = document.querySelector('#videoYoutube iframe');
+        const testo = document.getElementById('xDDD');
+		if(panel.tempo !== ''){
+			testo.src = `${panel.tempo}&autoplay=1`;
+		}
+      });
+      RemovedMods.append(box);
+    }
+    if(panel.area === 'Extra'){
+      FooterContent.append(box);
+    }
+    
+    const h4LeftPanel = document.querySelector('#LeftPanel h4:nth-of-type(2)');
+    if(RemovedMods.innerHTML === ''){
+      h4LeftPanel.style.display = 'none';
+    }else{
+      h4LeftPanel.style.display = '';
+    }
+    
+    //Funções
+    download.addEventListener('click', ()=>{
+      window.open(panel.download, '_blank')
+    })
+  });
+}
